38775c97b13d50073869d45fe33c2aaa381e5ebf
patogonnet <pato.gonnet@gmail.com>
2025-01-22 19:08:27 -0300

    origin mologado

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -29,7 +29,7 @@ const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const corsOptions = {
-  origin: 'http://127.0.0.1:5173',
+  origin: 'http://localhost:5173',
   credentials: true, // Habilitar el intercambio de cookies y otros datos de autenticación
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders:
--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+/*     https: {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    }, */
+    host: '0.0.0.0', // Permite que el servidor escuche en todas las interfaces
+    port: 5173,      // El puerto puede ser cambiado si es necesario
+  },
   plugins: [react()],
 })
